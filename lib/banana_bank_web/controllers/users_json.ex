eb79f6752ba184c035f79b236d7977f57cb6b957
Bruno Correa <bruno.correa@justtraveltour.com>
2024-12-15 11:07:30 -0300

    feat: use @derive jason

--- lib/banana_bank_web/controllers/users_json.ex
+++ lib/banana_bank_web/controllers/users_json.ex
@@ -1,21 +1,10 @@
 defmodule BananaBankWeb.UsersJSON do
-  alias BananaBank.Users.User
-
   def create(%{user: user}) do
     %{
       message: "User created successfully",
-      data: data(user)
+      data: user
     }
   end
 
-  def get(%{user: user}), do: %{data: data(user)}
-
-  defp data(%User{} = user) do
-    %{
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      cep: user.cep
-    }
-  end
+  def get(%{user: user}), do: %{data: user}
 end
