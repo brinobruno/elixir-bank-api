4eff9311d78eae2cb5a9448fb5cc6a169ea8b517
Bruno Correa <bruno.correa@justtraveltour.com>
2024-12-15 13:10:04 -0300

    test: controller delete test

--- test/banana_bank_web/users_controller_test.exs
+++ test/banana_bank_web/users_controller_test.exs
@@ -1,18 +1,28 @@
 defmodule BananaBankWeb.UsersControllerTest do
   use BananaBankWeb.ConnCase
 
-  describe "create" do
-    test "successfully creates an user", %{conn: conn} do
-      params = %{
-        name: "Bruno",
-        email: "bruno@bruno.com",
-        password: "coxinha123",
-        cep: "12233170"
-      }
+  alias BananaBank.Users
+  alias Users.User
 
+  @valid_params %{
+    name: "Bruno",
+    email: "bruno@bruno.com",
+    password: "coxinha123",
+    cep: "12233170"
+  }
+
+  @invalid_params %{
+    name: nil,
+    email: "bruno@bruno.com",
+    password: "coxinha123",
+    cep: "12"
+  }
+
+  describe "create/2" do
+    test "successfully creates an user", %{conn: conn} do
       response =
         conn
-        |> post(~p"/api/users", params)
+        |> post(~p"/api/users", @valid_params)
         |> json_response(:created)
 
       assert %{
@@ -27,21 +37,29 @@ defmodule BananaBankWeb.UsersControllerTest do
     end
 
     test "returns an error when attempting to create user with invalid params", %{conn: conn} do
-      params = %{
-        name: nil,
-        email: "bruno@bruno.com",
-        password: "coxinha123",
-        cep: "12"
-      }
-
       response =
         conn
-        |> post(~p"/api/users", params)
+        |> post(~p"/api/users", @invalid_params)
         |> json_response(:bad_request)
 
       expected_response = %{
         "errors" => %{"cep" => ["should be 8 character(s)"], "name" => ["can't be blank"]}
       }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "successfully deletes an user", %{conn: conn} do
+      {:ok, %User{id: id}} = Users.create(@valid_params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{id}")
+        |> json_response(:ok)
+
+      expected_response = %{"message" => "User deleted successfully"}
 
       assert response == expected_response
     end
