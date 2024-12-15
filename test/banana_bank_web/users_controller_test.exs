fe9cdd6ccd401adbc07a670c03ee1deec6afdfa2
Bruno Correa <bruno.correa@justtraveltour.com>
2024-12-15 12:56:47 -0300

    test: controller create test - should fail

--- test/banana_bank_web/users_controller_test.exs
+++ test/banana_bank_web/users_controller_test.exs
@@ -25,5 +25,25 @@ defmodule BananaBankWeb.UsersControllerTest do
                "message" => "User created successfully"
              } = response
     end
+
+    test "returns an error when attempting to create user with invalid params", %{conn: conn} do
+      params = %{
+        name: nil,
+        email: "bruno@bruno.com",
+        password: "coxinha123",
+        cep: "12"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+      expected_response = %{
+        "errors" => %{"cep" => ["should be 8 character(s)"], "name" => ["can't be blank"]}
+      }
+
+      assert response == expected_response
+    end
   end
 end
