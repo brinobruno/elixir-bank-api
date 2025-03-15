c749d8427bc8034ea94bba9cdec5c2eebc2ea24d
Bruno Correa <brunosantos6ft@gmail.com>
2025-03-14 21:59:07 -0300

    fix: unique constraint and create with existing user

--- lib/banana_bank/accounts/create.ex
+++ lib/banana_bank/accounts/create.ex
@@ -1,10 +1,23 @@
 defmodule BananaBank.Accounts.Create do
+  import Ecto.Query
+
   alias BananaBank.Accounts.Account
+  alias BananaBank.Users.User
   alias BananaBank.Repo
 
   def call(params) do
-    params
-    |> Account.changeset()
-    |> Repo.insert()
+    user = Repo.one(from u in User, where: u.id == ^params["user_id"])
+
+    case user do
+      nil ->
+        # Creates an empty changeset
+        changeset = Ecto.Changeset.change(%Account{})
+        {:error, Ecto.Changeset.add_error(changeset, :user_id, "User not found")}
+
+      _user ->
+        params
+        |> Account.changeset()
+        |> Repo.insert()
+    end
   end
 end
